76b1245d9f894d9a012a4ef676810ff7b33291c0
praveen6289 <kuthadipraveen366@gmail.comgit config --global user.name praveen6289>
2022-07-01 12:59:11 +0000

    Updated solution for browser history app

--- src/components/BrowserHistory/index.js
+++ src/components/BrowserHistory/index.js
@@ -4,9 +4,85 @@ import HistoryItem from '../HistoryItem'
 
 import './index.css'
 
+const initialHistoryList = [
+  {
+    id: 0,
+    timeAccessed: '07:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/instagram-img.png',
+    title: 'Instagram',
+    domainUrl: 'instagram.com',
+  },
+  {
+    id: 1,
+    timeAccessed: '05:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/twitter-img.png',
+    title: 'Twitter. It’s what’s happening / Twitter',
+    domainUrl: 'twitter.com',
+  },
+  {
+    id: 2,
+    timeAccessed: '04:35 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/facebook-img.png',
+    title: 'Facebook – log in or sign up',
+    domainUrl: 'facebook.com',
+  },
+  {
+    id: 3,
+    timeAccessed: '04:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/linkedin-img.png',
+    title: 'LinkedIn: Log In or Sign Up',
+    domainUrl: 'linkedin.com',
+  },
+  {
+    id: 4,
+    timeAccessed: '04:00 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/hashnode-img.png',
+    title: 'Hashnode: Everything you need to start blogging as a developer!',
+    domainUrl: 'hashnode.com',
+  },
+  {
+    id: 5,
+    timeAccessed: '03:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/github-img.png',
+    title: 'GitHub: Where the world builds software · GitHub',
+    domainUrl: 'github.com',
+  },
+
+  {
+    id: 6,
+    timeAccessed: '02:45 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/react-img.png',
+    title: 'React – A JavaScript library for building user interfaces',
+    domainUrl: 'reactjs.org',
+  },
+  {
+    id: 7,
+    timeAccessed: '01:25 PM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/stackoverflow-img.png',
+    title: 'Stack Overflow - Where Developers Learn, Share, & Build Careers',
+    domainUrl: 'stackoverflow.com',
+  },
+
+  {
+    id: 8,
+    timeAccessed: '09:25 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/gmail-img.png',
+    title: 'Gmail',
+    domainUrl: 'mail.google.com',
+  },
+  {
+    id: 9,
+    timeAccessed: '09:00 AM',
+    logoUrl: 'https://assets.ccbp.in/frontend/react-js/google-img.png',
+    title: 'Google',
+    domainUrl: 'google.com',
+  },
+]
+
 class BrowserHistory extends Component {
   state = {
     searchInput: '',
+    browserHistoryList: initialHistoryList,
   }
 
   onChangeSearchInput = event => {
@@ -15,10 +91,19 @@ class BrowserHistory extends Component {
     })
   }
 
+  onDeleteHistory = id => {
+    const {browserHistoryList} = this.state
+    const filteredHistoryList = browserHistoryList.filter(
+      eachHistory => eachHistory.id !== id,
+    )
+    this.setState({
+      browserHistoryList: filteredHistoryList,
+    })
+  }
+
   render() {
-    const {searchInput} = this.state
-    const {initialHistoryList} = this.props
-    const browserResults = initialHistoryList.filter(eachHistory =>
+    const {searchInput, browserHistoryList} = this.state
+    const browserResults = browserHistoryList.filter(eachHistory =>
       eachHistory.title.toLowerCase().includes(searchInput.toLowerCase()),
     )
     return (
@@ -47,11 +132,21 @@ class BrowserHistory extends Component {
           </div>
         </div>
         <div className="content-bg-container">
-          <ul className="list-container">
-            {browserResults.map(eachHistory => (
-              <HistoryItem historyDetails={eachHistory} key={eachHistory.id} />
-            ))}
-          </ul>
+          {browserResults.length > 0 ? (
+            <ul className="list-container">
+              {browserResults.map(eachHistory => (
+                <HistoryItem
+                  historyDetails={eachHistory}
+                  key={eachHistory.id}
+                  onDeleteHistory={this.onDeleteHistory}
+                />
+              ))}
+            </ul>
+          ) : (
+            <div className="empty-history-container">
+              <p className="empty-history">There is no history to show</p>
+            </div>
+          )}
         </div>
       </div>
     )
--- src/components/HistoryItem/index.js
+++ src/components/HistoryItem/index.js
@@ -1,25 +1,36 @@
 import './index.css'
 
 const HistoryItem = props => {
-  const {historyDetails} = props
-  const {timeAccessed, logoUrl, title, domainUrl} = historyDetails
+  const {historyDetails, onDeleteHistory} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyDetails
+
+  const onDeleteHistoryItem = () => {
+    onDeleteHistory(id)
+  }
 
   return (
     <li className="list-item">
       <p className="time">{timeAccessed}</p>
       <div className="history-container">
         <div className="history-details-container">
-          <img src={logoUrl} alt={title} className="img" />
+          <img src={logoUrl} alt="domain logo" className="img" />
           <div className="history-details">
             <p className="title">{title}</p>
             <p className="url">{domainUrl}</p>
           </div>
         </div>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
-          alt="delete"
-          className="delete-img"
-        />
+        <button
+          className="delete-button"
+          type="button"
+          onClick={onDeleteHistoryItem}
+          testid="delete"
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            alt="delete"
+            className="delete-img"
+          />
+        </button>
       </div>
     </li>
   )
